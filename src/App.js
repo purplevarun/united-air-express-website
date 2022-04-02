1a89aafec7159651e3f0183674941cf968d961bd
purplevarun <purplevarun@gmail.com>
2022-04-02 21:01:23 +0530

    Added Routing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./pages/About/About";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>united air express Asansol</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Homepage />} path="/" exact />
+					<Route element={<About />} path="/about" exact />
+					<Route element={<Homepage />} path="*" />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
