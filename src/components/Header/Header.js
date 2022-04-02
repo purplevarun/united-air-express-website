777adcea3b4e1285403bfc7ac50bbcb3d32fed30
purplevarun <purplevarun@gmail.com>
2022-04-02 22:05:16 +0530

    Created Menu Icons for Desktop AppBar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,12 @@
-import {
-	AppBar,
-	Button,
-	IconButton,
-	Menu,
-	MenuItem,
-	Toolbar,
-	Typography,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { AppBar, IconButton, Menu, MenuItem, Typography } from "@mui/material";
+import React, { useState } from "react";
 import "./Header.css";
 import { FiPackage } from "react-icons/fi";
 import { useMediaQuery } from "react-responsive";
 import { TiThMenuOutline } from "react-icons/ti";
+import { RiHome2Fill, RiFolderInfoFill } from "react-icons/ri";
+import { FaNewspaper } from "react-icons/fa";
+import { IoMdPhotos } from "react-icons/io";
 const Header = () => {
 	const [openMenu, setOpenMenu] = useState(false);
 	const toggleMobileMenu = (event) => {
@@ -38,27 +33,66 @@ const Header = () => {
 					anchorEl={openMenu}
 					onClose={closeMobileMenu}
 				>
-					<MenuItem onClick={closeMobileMenu}>Profile</MenuItem>
-					<MenuItem onClick={closeMobileMenu}>My account</MenuItem>
-					<MenuItem onClick={closeMobileMenu}>Logout</MenuItem>
+					<MenuItem onClick={closeMobileMenu}>Home</MenuItem>
+					<MenuItem onClick={closeMobileMenu}>Apply</MenuItem>
+					<MenuItem onClick={closeMobileMenu}>Gallery</MenuItem>
+					<MenuItem onClick={closeMobileMenu}>About</MenuItem>
 				</Menu>
 			</>
 		);
 	};
 	const renderDesktopMenu = () => {
-		return <h1>desktop</h1>;
+		return (
+			<>
+				<IconButton
+					color="inherit"
+					size="large"
+					style={{ borderRadius: "0", marginRight: "10px" }}
+				>
+					<RiHome2Fill />
+					&nbsp; Menu
+				</IconButton>
+				<IconButton
+					color="inherit"
+					size="large"
+					style={{ borderRadius: "0", marginRight: "10px" }}
+				>
+					<FaNewspaper />
+					&nbsp; Apply
+				</IconButton>
+				<IconButton
+					color="inherit"
+					size="large"
+					style={{ borderRadius: "0", marginRight: "10px" }}
+				>
+					<IoMdPhotos />
+					&nbsp; Gallery
+				</IconButton>
+				<IconButton
+					color="inherit"
+					size="large"
+					style={{ borderRadius: "0", marginRight: "10px" }}
+				>
+					<RiFolderInfoFill /> &nbsp; About
+				</IconButton>
+			</>
+		);
 	};
 	return (
 		<div className="header">
 			<AppBar position="fixed" color="secondary">
 				<div className="appbar">
 					<div className="left">
-						<IconButton color="inherit">
+						<IconButton
+							color="inherit"
+							style={{ borderRadius: "0" }}
+						>
 							<FiPackage />
+							&nbsp;
+							<Typography variant="h5" align="center">
+								United Air Express
+							</Typography>
 						</IconButton>
-						<Typography variant="h5" align="center">
-							United Air Express
-						</Typography>
 					</div>
 					<div className="right">
 						{isMobile ? renderMobileMenu() : renderDesktopMenu()}
