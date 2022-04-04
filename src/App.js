fa64b540b5e8c48e99429498d56d364d0214fe24
purplevarun <purplevarun@gmail.com>
2022-04-04 22:21:30 +0530

    Added defaultTheme functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import Router from "./router/Router";
 import lightTheme from "./themes/lightTheme";
 import darkTheme from "./themes/darkTheme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
 	const [darkMode, setDarkMode] = useState(false);
+	useEffect(() => {
+		const defaultTheme = localStorage.getItem("defaultTheme");
+		if (defaultTheme) {
+			if (defaultTheme === "light") {
+				setDarkMode(false);
+			} else {
+				setDarkMode(true);
+			}
+		}
+	}, []);
+	const setDarkModeAndSave = () => {
+		setDarkMode(!darkMode);
+		if (darkMode) {
+			localStorage.setItem("defaultTheme", "light");
+		} else {
+			localStorage.setItem("defaultTheme", "dark");
+		}
+	};
 	return (
 		<div className="App">
 			<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
 				<CssBaseline />
-				<Router darkMode={darkMode} setDarkMode={setDarkMode} />
+				<Router darkMode={darkMode} setDarkMode={setDarkModeAndSave} />
 			</ThemeProvider>
 		</div>
 	);
