b2f298f890c91266f9849246b0c5a801c6d24127
purplevarun <purplevarun@gmail.com>
2022-04-07 22:41:09 +0530

    Extracted Homepage Slider component

---
+++ src/components/ApplySlider/ApplySlider.js
@@ -0,0 +1,46 @@
+import { Slide, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./ApplySlider.css";
+const ApplySlider = () => {
+	const navigate = useNavigate();
+	const gotoApplypage = () => {
+		navigate("/apply");
+	};
+
+	const [growHeader, setGrowHeader] = useState(false);
+	const [growText, setGrowText] = useState(false);
+	useEffect(() => {
+		setTimeout(() => {
+			setGrowHeader(true);
+		}, 1000);
+		setTimeout(() => {
+			setGrowText(true);
+		}, 3000);
+	}, []);
+	return (
+		<div className="animated-text">
+			<Typography variant="h6" fontWeight="600">
+				Welcome to
+			</Typography>
+			<Slide in={growHeader} direction="up">
+				<Typography variant="h4" color="red">
+					United Air Express
+				</Typography>
+			</Slide>
+			<div className="animated-text-loop">
+				<Slide in={growText} direction="left">
+					<Typography variant="h5">
+						Click{" "}
+						<span className="clickable" onClick={gotoApplypage}>
+							here
+						</span>{" "}
+						to apply for job
+					</Typography>
+				</Slide>
+			</div>
+		</div>
+	);
+};
+
+export default ApplySlider;
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useMediaQuery } from "react-responsive";
 import "./Homepage.css";
 import bgimg from "./../../images/background.jpg";
 import uae_front from "./../../images/homepage_bg.jpg";
-import { Slide, Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+
+import ApplySlider from "../../components/ApplySlider/ApplySlider";
 const Homepage = () => {
 	const isMobile = useMediaQuery({
 		query: "(max-width: 786px)",
@@ -31,56 +31,7 @@ const Homepage = () => {
 			</div>
 		);
 	};
-	const navigate = useNavigate();
-	const gotoApplypage = () => {
-		navigate("/apply");
-	};
-	const gotoAboutpage = () => {
-		navigate("/about");
-	};
-	const gotoGallerypage = () => {
-		navigate("/gallery");
-	};
-	const [growText, setGrowText] = useState({
-		first: false,
-		second: false,
-		third: false,
-		fourth: false,
-	});
-	useEffect(() => {
-		setTimeout(() => {
-			setGrowText({
-				first: true,
-				second: false,
-				third: false,
-				fourth: false,
-			});
-			setTimeout(() => {
-				setGrowText({
-					first: true,
-					second: true,
-					third: false,
-					fourth: false,
-				});
-				setTimeout(() => {
-					setGrowText({
-						first: true,
-						second: true,
-						third: true,
-						fourth: false,
-					});
-					setTimeout(() => {
-						setGrowText({
-							first: true,
-							second: true,
-							third: true,
-							fourth: true,
-						});
-					}, 2000);
-				}, 2000);
-			}, 2000);
-		}, 2000);
-	}, []);
+
 	return (
 		<div
 			className="homepage"
@@ -94,48 +45,7 @@ const Homepage = () => {
 					? renderFrontImageMobile()
 					: renderFrontImageDesktop()}
 			</>
-			<div className="animated-text">
-				<Typography variant="h6" fontWeight="600">
-					Welcome to
-				</Typography>
-				<Slide in={growText.first} direction="up">
-					<Typography variant="h4" color="red">
-						United Air Express
-					</Typography>
-				</Slide>
-				<div className="animated-text-loop">
-					<Slide in={growText.second} direction="left">
-						<Typography variant="h5">
-							Click{" "}
-							<span className="clickable" onClick={gotoApplypage}>
-								here
-							</span>{" "}
-							to apply for job
-						</Typography>
-					</Slide>
-					<Slide in={growText.third} direction="right">
-						<Typography variant="h5">
-							Click{" "}
-							<span
-								className="clickable"
-								onClick={gotoGallerypage}
-							>
-								here
-							</span>{" "}
-							to check out our office
-						</Typography>
-					</Slide>
-					<Slide in={growText.fourth} direction="left">
-						<Typography variant="h5">
-							Click{" "}
-							<span className="clickable" onClick={gotoAboutpage}>
-								here
-							</span>{" "}
-							to know more about us
-						</Typography>
-					</Slide>
-				</div>
-			</div>
+			<ApplySlider />
 		</div>
 	);
 };
